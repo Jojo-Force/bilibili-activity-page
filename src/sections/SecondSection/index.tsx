5ee7b21939fc10bf7b183aec4085c7e4fd14b829
jojo <jojo@duolin.com>
2025-10-22 15:43:07 +0800

    1.完成滚动tab吸顶

--- src/sections/SecondSection/index.tsx
+++ src/sections/SecondSection/index.tsx
@@ -1,10 +1,11 @@
-import {FC, useState} from "react";
+import {FC, useEffect, useRef, useState} from "react";
 import CartoonImage from '../../assets/cartoon.jpg';
 import MovieImage from '../../assets/movie.png';
 import FoodImage from '../../assets/food.jpg';
 import LifeImage from '../../assets/life.jpg';
 import styles from './styles.module.scss'
 import classNames from "classnames";
+import { tab } from "@testing-library/user-event/dist/tab";
 
 const tabs = [
     {
@@ -24,15 +25,47 @@ const tabs = [
         title: '生活',
     }
 ]
+
+//1. 点击 Tab 滚动跳转
+//2. 滚动时，高亮Tab
+//3. Tabs 吸顶
+//4. 按钮吸底
 const SecondSection: FC = ()=>{
     const [activeTab, setActiveTab] = useState<string>('cartoon');
+    const [isFixed, setIsFixed] = useState<boolean>(false);
+    const secondSectionRef =  useRef<HTMLDivElement>(null);
+
+    const active = (key: string) => {
+        setActiveTab(key);
+
+        const tabContentEl = document.querySelector(`[data-id=${key}]`);//找到对应id的section
+
+        if(tabContentEl){
+            tabContentEl.scrollIntoView({ behavior: 'smooth'});
+        }
+    }
+
+    const onScroll = () => {
+        if(secondSectionRef.current){
+            const {top} = secondSectionRef.current.getBoundingClientRect();
+            setIsFixed(top <= 0);
+        }
+    }
+
+    useEffect(()=>{
+        window.addEventListener('scroll', onScroll);
+
+        return () => {
+            window.removeEventListener('scroll', onScroll);
+        }
+    },[])
 
     return (
-        <div className={styles.secondSection}>
+        <div className={styles.secondSection} ref={secondSectionRef}>
             {/* Tabs */}
-            <ul>
+            <ul className={classNames({[styles.isFixed]: isFixed})}>
                 {tabs.map(tab => (
-                    <li key={tab.key} onClick={() => setActiveTab(tab.key)}>
+                    <li key={tab.key} onClick={() => active(tab.key)}>
                     <span>{tab.title}</span>
                     <span className={classNames(styles.line, {[styles.visible]: activeTab === tab.key})} />
                     </li>
@@ -40,22 +73,13 @@ const SecondSection: FC = ()=>{
 
             </ul>
             {/* Tab　Content */}
-            <section>
-                <h2>动画</h2>
-                <img src={CartoonImage} alt="Cartoon" />
-            </section>
-            <section>
-                <h2>美食</h2>
-                <img src={FoodImage} alt="Food" />
-            </section>
-            <section>
-                <h2>电影</h2>
-                <img src={MovieImage} alt="Movie" />
-            </section>
-            <section>
-                <h2>生活</h2>
-                <img src={LifeImage} alt="Life" />
-            </section>
+
+            {tabs.map(tab=>(
+                <section data-id={tab.key}>
+                    <h2>{tab.title}</h2>
+                    <img src={CartoonImage} alt={tab.key} />
+                </section>
+            ))}
 
         </div>
     )
